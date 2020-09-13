de88f33d65afde965c57e904d7b8aeb32749ba5c
debashree95 <debashreepaul95@gmail.com>
2020-09-13 12:25:40 +0530

    initial commit

---
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PromotionEngineTest
+{
+    public class PromotionEngineTest
+    {
+        [Fact]
+        public void NullValueTest()
+        {
+
+        }
+    }
+}
