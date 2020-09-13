fd521845e5d97613f1cdd33c5ea667545fad2d75
debashree95 <debashreepaul95@gmail.com>
2020-09-13 22:33:43 +0530

    Test Case to Validate Product Type

--- Promotion Engine/Program.cs
+++ Promotion Engine/Program.cs
@@ -7,92 +7,9 @@ namespace Promotion_Engine
 {
     class Program
     {
-      
 
-        #region  Methods
-        static void Main(string[] args)
-        {
-            List<Product> products = new List<Product>();
 
-            Console.WriteLine("total number of order");
-            int a = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < a; i++)
-            {
-                Console.WriteLine("enter the type of product:A,B,C or D");
-                string type = Console.ReadLine();
-                Product p = GetProductPrice(type);
-                products.Add(p);
-            }
-
-            long totalPrice = CalculateTotalPrice(products);
-            Console.WriteLine(totalPrice);
-            Console.ReadLine();
-
-        }
-        public static Product GetProductPrice(string type)
-        {
-            Product item = new Product();
-            item.Id = type;
-            switch (type)
-            {
-                case "A":
-                    item.Price = 50m;
-
-                    break;
-                case "B":
-                    item.Price = 30m;
-
-                    break;
-                case "C":
-                    item.Price = 20m;
-
-                    break;
-                case "D":
-                    item.Price = 2015m;
-                    break;
-            }
-            return item;
-
-        }
-        public static long CalculateTotalPrice(List<Product> products)
-        {
-            int counterofA = 0;
-            int priceofA = 50;
-            int counterofB = 0;
-            int priceofB = 30;
-            int CounterofC = 0;
-            int priceofC = 20;
-            int CounterofD = 0;
-            int priceofD = 15;
-            foreach (Product pr in products)
-            {
-                if (pr.Id == "A" || pr.Id == "a")
-                {
-                    counterofA = counterofA + 1;
-                }
-                if (pr.Id == "B" || pr.Id == "b")
-                {
-                    counterofB = counterofB + 1;
-                }
-                if (pr.Id == "C" || pr.Id == "c")
-                {
-                    CounterofC = CounterofC + 1;
-                }
-                if (pr.Id == "D" || pr.Id == "d")
-                {
-                    CounterofD = CounterofD + 1;
-                }
-            }
-            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
-            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
-            int totalPriceofC = (CounterofC * priceofC);
-            int totalPriceofD = (CounterofD * priceofD);
-            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
-
-        }
     }
-
-    #endregion
 
 }
 
--- PromotionEngineTest/PromotionEngineTest.cs
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Promotion_Engine;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -8,9 +10,16 @@ namespace PromotionEngineTest
     public class PromotionEngineTest
     {
         [Fact]
-        public void NullValueTest()
+        public void Invalid_Product_Type_Test()
         {
-            //
+            //Arrange
+            string type = "F";
+        
+            //Act
+            var response= PromotionClass.GetProductPrice(type);
+
+            //Assert
+            Assert.Equal(0,response.Price);
 
         }
     }
